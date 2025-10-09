48e261b6a1d7cb6e8dad644769717c9a5294f585
habtamneshmarye <habtamneshmarye@gmail.com>
2025-10-09 18:24:49 +0300

    added mobile documentation

--- halicare-documentation/src/app/page.tsx
+++ halicare-documentation/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HomePage from "./home/page";
+import MobileDocumentationPage from "./frontend-mobile/page";
 
 export default function Home() {
   return (
     <div className="">
 <HomePage/>
+<MobileDocumentationPage/>
 
     </div>
   );
